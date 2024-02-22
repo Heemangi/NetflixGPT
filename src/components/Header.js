df96d917ddd4572c9e08f6345423535664fb84e5
Heemangi <rupaniheemangi5@gmail.com>
2024-02-22 11:23:28 +0530

    GPT Page Setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
 import { LOGO } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -35,12 +36,20 @@ const Header = () => {
       return () => unsubscribe();
 }, []);
 
+  const handleGptSearchClick = () =>{
+  //Toggling the GPT Search
+    dispatch(toggleGptSearchView());
+  }
+
   return (
     <div className="logopositioning absolute justify-between flex w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
         <img className="logosizing w-44"
         src={LOGO}
         alt="logo"/>
         {user && (<div className="flex p-2 space-x-2">
+        <button className="py-2 px-4 mx-4 bg-red-400 text-white rounded-lg h-12"
+        onClick={handleGptSearchClick}
+        >GPT Mode</button>
         <img className="w-12 h-12" alt="usericon" src={user?.photoURL}/>
         <button onClick={handleSignOut} className="text-white font-bold">Sign Out</button>
         </div>)}
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, up
 import { auth } from "../utils/firebase";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
-import { USER_AVATAR } from "../utils/constants";
+import { BG_URL, USER_AVATAR } from "../utils/constants";
 
 const Login = () => {
     const [isSignInForm, setIsSignInForm] = useState(true);
@@ -74,8 +74,7 @@ const Login = () => {
     <div>
         <Header/>
         <div className="absolute">
-            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/5e16108c-fd30-46de-9bb8-0b4e1bbbc509/29d8d7d7-83cc-4b5f-aa9b-6fd4f68bfaa6/IN-en-20240205-popsignuptwoweeks-perspective_alpha_website_small.jpg"
-            alt="banner"/>
+            <img src={BG_URL} alt="banner"/>
         </div>
         <form onSubmit={(e) => e.preventDefault() } className=" text-white w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 bg-opacity-80 rounded-md">
             <h1 className="font-bold text-3xl py-4"> {isSignInForm ? "Sign In" : "Sign Up"}</h1>
