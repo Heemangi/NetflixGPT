29f82eb40ad81f9622d04d73627f2dadd5be5cff
Heemangi <rupaniheemangi5@gmail.com>
2024-02-26 10:58:33 +0530

    GPT SearchBar and Multi Language Functionality

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import lang from '../utils/languageConstants'
+import { useSelector } from 'react-redux'
 
 const GptSearchBar = () => {
+    const langKey = useSelector((store) => store.config.lang);
+
   return (
     <div className='pt-[10%] flex justify-center'>
         <form className='w-1/2 bg-black grid grid-cols-12'>
             <input type='text' className='p-4 m-4 col-span-9 rounded-lg' 
-            placeholder='What would you like to watch today ? '/>
-            <button className=' col-span-3 m-4 py-2 px-4 bg-red-600 text-white rounded-lg  '>Search</button>
+            placeholder={lang[langKey].gptSearchPlaceholder}/>
+            {/**lang[langKey] to make it dynamic since langkey is not defined anywhere in slice*/}
+            <button className=' col-span-3 m-4 py-2 px-4 bg-red-600 text-white rounded-lg  '>
+                {lang[langKey].search}</button>
         </form>
     </div>
   )
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,17 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO } from "../utils/constants";
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
 import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch= useDispatch();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
+
   const handleSignOut = () => {
     signOut(auth).then(() => {
     }).catch((error) => {
@@ -40,6 +44,13 @@ const Header = () => {
   //Toggling the GPT Search
     dispatch(toggleGptSearchView());
   }
+  
+  //onchange will trigger an event 
+  const handleLanguageChange = (e) => {
+    //console.log(e.target.value);
+    dispatch(changeLanguage(e.target.value));
+  }
+
 
   return (
     <div className="logopositioning absolute justify-between flex w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
@@ -47,9 +58,16 @@ const Header = () => {
         src={LOGO}
         alt="logo"/>
         {user && (<div className="flex p-2 space-x-2">
+          {showGptSearch && <select className=" rounded-lg p-2 bg-gray-900 text-white mx-2 h-12" onChange={handleLanguageChange}>
+            {SUPPORTED_LANGUAGES.map(lang => <option key={lang.identifier} value = {lang.identifier}>{lang.name}</option> )}
+          </select>}
+          {/* <select> // Only if ShowGPTSearch is true
+            <option value = "en">English</option>
+            <option value = "hindi">Hindi</option>
+          </select> */}
         <button className="py-2 px-4 mx-4 bg-red-400 text-white rounded-lg h-12"
         onClick={handleGptSearchClick}
-        >GPT Mode</button>
+        >{showGptSearch ? "Home Page" : "GPT Mode"}</button>
         <img className="w-12 h-12" alt="usericon" src={user?.photoURL}/>
         <button onClick={handleSignOut} className="text-white font-bold">Sign Out</button>
         </div>)}
